4c6d2d46b0049178491abd9e93d19bafebdfd686
Medamine97 <mabenfatma97@gmail.com>
2025-04-08 11:13:11 +0100

    Virtual Network & Subnet provision

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,17 @@ resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "azurerm_virtual_network" "main_vnet" {
+  name                = "main-vnet"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
